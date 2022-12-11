6e37e7c5207d084260c0090d0e26a18813e8539c
Derrick Hodges <hodgesd@gmail.com>
2022-12-11 14:54:54 -0600

    initial

---
+++ jsSwiftBar.js
@@ -0,0 +1,15 @@
+#!/usr/local/bin/node
+/*
+* <xbar.title>jsSwiftBar</xbar.title>
+* <xbar.version>v1.0</xbar.version>
+* <xbar.author>hodgesd</xbar.author>
+* <xbar.author.github>hodgesd</xbar.author.github>
+* <xbar.desc>Test SwiftBar Plugin</xbar.desc>
+* <xbar.dependencies>node</xbar.dependencies>
+* <xbar.abouturl>http://varunmalhotra.xyz/blog/2016/02/bitbar-plugins-for-github-and-producthunt.html</xbar.abouturl>
+*/
+
+
+console.log("js"+'\n---\n')
+console.log("Hello, World")
+
