82d4d99f3e8dfb8279fc5e514c02365373d92f9f
Derrick Hodges <hodgesd@gmail.com>
2022-12-13 09:13:18 -0600

    aafes:  added commenting

--- aafesDOTD.3600m.js
+++ aafesDOTD.3600m.js
@@ -12,8 +12,17 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-// url = 'https://www.shopmyexchange.com/s?Dy=1&Nty=1&Ntt=dotd';
-url = 'https://www.shopmyexchange.com/s?Dy=1&Nty=1&Ntt=DealoftheDay13December';
+// Get the current date in the format DMonthName
+// e.g. 13December
+// todo: add leading zero to day if day < 10?
+const date = new Date();
+let day = date.getDate();
+let monthName = date.toLocaleString('default', { month: 'long' });
+
+// console.log(`${day}${monthName}`);
+
+url = `https://www.shopmyexchange.com/s?Dy=1&Nty=1&Ntt=DealoftheDay${day}${monthName}`;
+// url = 'https://www.shopmyexchange.com/s?Dy=1&Nty=1&Ntt=DealoftheDay13December';
 
 // Function to get the DOM of a webpage
 async function getDOM(url) {
@@ -28,29 +37,27 @@ console.log(
 );
 
 getDOM(url).then((dom) => {
-  // Find all the elements with the class "clus"
+  // Find all DOTD items on sale
   const items = dom.window.document.querySelectorAll('.aafes-thumbnail-item');
   const itemsArray = Array.from(items);
-  // Loop through the stories and output the headline
+  // Loop through the items and output the item name, sale price, discount, and link
   itemsArray.forEach((item) => {
     const itemName = item
       .querySelector('.aafes-item-name')
       .querySelector('a')
       .textContent.trim();
-    // console.log(itemName);
-
     const itemSalePrice = item
       .querySelector('.item-pricing')
       .querySelector('.aafes-price-sale')
       .textContent.trim()
-      .slice(0, -5);
+      .slice(0, -5); // remove the "Sale" from the end of the price
     const itemDiscount = item
       .querySelector('.aafes-price-saved')
       .textContent.trim()
-      .slice(-4, -1);
+      .slice(-4, -1); // get the discount percentage... always 2 digits?
     const itemLink = item.querySelector('a').href;
     console.log(
-      `${itemSalePrice}[-${itemDiscount}] ${itemName} | href= ${itemLink} length= 80`
+      `${itemSalePrice}[-${itemDiscount}] ${itemName} | href= ${itemLink} length= 90`
     );
   });
 });
