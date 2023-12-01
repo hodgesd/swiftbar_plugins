fd49498ff62717b4e294768b65031d0b64129ac6
hodgesd <hodgesd@gmail.com>
2023-12-01 13:26:12 -0600

    Convert techmeme script from JavaScript to Python
    The techmeme script was switched from JavaScript to Python, including changes in dependency and the way the DOM is handled. This refactor provides maintainability advantages due to the simplicity of the new configuration and implementation.

---
+++ techmeme.60m.py
@@ -0,0 +1,46 @@
+#!/Users/hodgesd/PycharmProjects/swiftbar_plugins/venv/bin/python3
+"""
+<xbar.title>jsSwiftBar</xbar.title>
+<xbar.version>v1.0</xbar.version>
+<xbar.author>hodgesd</xbar.author>
+<xbar.author.github>hodgesd</xbar.author.github>
+<xbar.desc>Test SwiftBar Plugin</xbar.desc>
+<xbar.dependencies>python</xbar.dependencies>
+<xbar.abouturl>http://varunmalhotra.xyz/blog/2016/02/bitbar-plugins-for-github-and-producthunt.html</xbar.abouturl>
+"""
+
+# <swiftbar.hideAbout>true</swiftbar.hideAbout>
+# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
+# <swiftbar.hideLastUpdated>true</swiftbar.hideLastUpdated>
+# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
+# <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
+
+import asyncio
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# Function to get the DOM of a webpage
+async def getDOM(url):
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+
+url = 'https://www.techmeme.com/'
+
+result = asyncio.run(getDOM(url))
+stories = result.select('.clus')
+
+print('TM' + '\n---\n')
+# log link to techmeme.com
+print('Techmeme | href= https://www.techmeme.com/' + '\n---\n')
+
+for story in stories:
+    try:
+        story_site = story.select('cite')[0].select('a')[0].text
+        story_link = story.select('.ourh')[0]['href']
+        story_title = story.select('.ourh')[0].text
+    except Exception:
+        pass
+    print(f'{story_title}[{story_site}] | href= {story_link} length= 90')
