481b0cdac49994572a8a9927e266af7b0796b372
hodgesd <hodgesd@gmail.com>
2024-02-18 11:47:41 -0600

    Modify print statement for hyperlink formatting in 'techmeme.60m.py'
    This commit adjusts the 'print' method used to display URLs in order to streamline the format across the code. Additionally, the change incorporates 'trim=True tooltip={story_title}', improving the parsing of tipoff times and URL formatting.

--- techmeme.60m.py
+++ techmeme.60m.py
@@ -46,4 +46,4 @@ for story in stories:
         pass
     # display_title = story_title if len(story_title) <= 70 else story_title[:67] + '...'
     # print(f'[{story_site}] {story_title}| href= {story_link} length=90 trim=True tooltip={story_title}')
-    print(f'[{story_site}] {story_title} | href={story_link} length=LINE_LENGTH')
+    print(f'[{story_site}] {story_title} | href={story_link} length=LINE_LENGTH trim=True tooltip={story_title}')
