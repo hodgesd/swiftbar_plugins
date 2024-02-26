79a1e43bb17f1122bddb3d35b453efabebe354b4
hodgesd <hodgesd@gmail.com>
2024-02-26 09:25:24 -0600

    Update 'discourse_tops.120m.py' to ignore specific categories
    The script 'discourse_tops.120m.py' is now updated to add functionality to ignore specified categories from specific forums. The 'ignore categories' feature is now included in 'get_discourse_posts' function. For instance, "Bug reports" category from "Obsidian" and

--- discourse_tops.120m.py
+++ discourse_tops.120m.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S PATH="${PATH}:/usr/local/bin" python3
-#!unset TERM
+# !unset TERM
 
 # <bitbar.title>Discourse Top Posts</bitbar.title>
 # <bitbar.version>v0.8</bitbar.version>
@@ -18,38 +18,48 @@
 # Todo: add star for posts with high like counts (heatmap ≥ low)
 # Todo: add support to exclude categories 
 
-import requests
 import pandas as pd
-
+import requests
 
 MAX_POSTS = 15
-
 
 forums = {
     "Automators": "https://talk.automators.fm/top?period=weekly",
     "Obsidian": "https://forum.obsidian.md/top?period=weekly",
-    "Level1Techs":"https://forum.level1techs.com/top?period=weekly",
-    "Drafts":"https://forums.getdrafts.com/top?period=monthly",
-    "Traefik":"https://community.traefik.io/top?period=weekly",
-    "Pi-hole":"https://discourse.pi-hole.net/top?period=weekly",
-    "Home Assistant":"https://community.home-assistant.io/top?period=weekly",
-    "Keyboard Maestro":"https://forum.keyboardmaestro.com/top?period=weekly",
+    "Level1Techs": "https://forum.level1techs.com/top?period=weekly",
+    "Drafts": "https://forums.getdrafts.com/top?period=monthly",
+    "Traefik": "https://community.traefik.io/top?period=weekly",
+    "Pi-hole": "https://discourse.pi-hole.net/top?period=weekly",
+    "Home Assistant": "https://community.home-assistant.io/top?period=weekly",
+    "Keyboard Maestro": "https://forum.keyboardmaestro.com/top?period=weekly",
     "Python": "https://discuss.python.org/top?period=weekly",
 }
 
+ignore_categories = {
+    "Obsidian": ["Bug reports"],
+    "Level1Techs": ["GPU"],
+}
 
-def get_discourse_posts(forum_url:str) -> pd:
+
+def get_discourse_posts(forum_url: str, ignored_categories: list = None) -> pd:
     r = requests.get(forum_url)
     url_html = r.text
     posts_df = pd.read_html(url_html, extract_links="all")[0]
     posts_df.columns = ["Headline", "Authors", "Replies", "Views", "Activity"]
-    posts_df[['Topic', 'Topic_url']] = pd.DataFrame(posts_df['Headline'].tolist(), index=posts_df.index)   
-    posts_df["Category"] = posts_df["Topic"].str.split("  ").str.get(1) # get text before the double space
-    posts_df["Topic"] = posts_df["Topic"].str.split("  ").str.get(0) # get text before the double space
-    posts_df["Headline"] = [f"{str(x)} [{z}]| href = {y}" for x, y, z in zip(posts_df['Topic'], posts_df['Topic_url'], posts_df['Category'])]
+    posts_df[['Topic', 'Topic_url']] = pd.DataFrame(posts_df['Headline'].tolist(), index=posts_df.index)
+    posts_df["Category"] = posts_df["Topic"].str.split("  ").str.get(1)  # get text before the double space
+    posts_df["Topic"] = posts_df["Topic"].str.split("  ").str.get(0)  # get text before the double space
+    posts_df["Headline"] = [f"{str(x)} [{z}]| href = {y}" for x, y, z in
+                            zip(posts_df['Topic'], posts_df['Topic_url'], posts_df['Category'])]
+
+    # Check if there are categories to ignore
+    if ignored_categories:
+        posts_df = posts_df[~posts_df['Category'].isin(ignored_categories)]
+
     return posts_df["Headline"].head(MAX_POSTS)
 
-def send_to_bitbar(forum_title:str, posts_df:pd, forum_url:str) -> None:
+
+def send_to_bitbar(forum_title: str, posts_df: pd, forum_url: str) -> None:
     print(f"{forum_title} | href = {forum_url}")
     posts_df.apply(lambda x: print(f"--{x}", end='\n'))
 
@@ -60,5 +70,6 @@ print("---")
 
 # Dropdown Menu - Top Posts from Discourse Forums
 for forum_title, forum_url in forums.items():
-    posts_df = get_discourse_posts(forum_url)
+    ignored_categories = ignore_categories.get(forum_title)
+    posts_df = get_discourse_posts(forum_url, ignored_categories)
     send_to_bitbar(forum_title, posts_df, forum_url)
